4db871826a5fc28e307c27e6d39b22defe35d5f4
irshal <irshalfir@gmail.com>
2019-11-25 13:19:04 +0700

    finish created balance administrator back-end and UI

--- proyek-kantin/balance/BalanceAdministratorForm.Designer.cs
+++ proyek-kantin/balance/BalanceAdministratorForm.Designer.cs
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tbUserNum = new System.Windows.Forms.TextBox();
+            this.tbBalance = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Btn_Confirm = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 153);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(402, 343);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tbUserNum
+            // 
+            this.tbUserNum.Location = new System.Drawing.Point(141, 37);
+            this.tbUserNum.Name = "tbUserNum";
+            this.tbUserNum.Size = new System.Drawing.Size(118, 20);
+            this.tbUserNum.TabIndex = 1;
+            // 
+            // tbBalance
+            // 
+            this.tbBalance.Location = new System.Drawing.Point(141, 75);
+            this.tbBalance.Name = "tbBalance";
+            this.tbBalance.Size = new System.Drawing.Size(118, 20);
+            this.tbBalance.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Tag = "";
+            this.label1.Text = "Nomor Induk";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nominal TopUp";
+            // 
+            // Btn_Confirm
+            // 
+            this.Btn_Confirm.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Btn_Confirm.FlatAppearance.BorderSize = 0;
+            this.Btn_Confirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Confirm.ForeColor = System.Drawing.Color.White;
+            this.Btn_Confirm.Location = new System.Drawing.Point(302, 70);
+            this.Btn_Confirm.Name = "Btn_Confirm";
+            this.Btn_Confirm.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Confirm.TabIndex = 4;
+            this.Btn_Confirm.Text = "Konfirmasi";
+            this.Btn_Confirm.UseVisualStyleBackColor = false;
+            this.Btn_Confirm.Click += new System.EventHandler(this.Btn_Confirm_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // BalanceAdministratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(426, 508);
+            this.Controls.Add(this.Btn_Confirm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbBalance);
+            this.Controls.Add(this.tbUserNum);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "BalanceAdministratorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BalanceAdministratorForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tbUserNum;
+        private System.Windows.Forms.TextBox tbBalance;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Btn_Confirm;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- proyek-kantin/balance/BalanceAdministratorForm.cs
+++ proyek-kantin/balance/BalanceAdministratorForm.cs
@@ -7,14 +7,123 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace proyek_kantin.balance
 {
     public partial class BalanceAdministratorForm : Form
     {
+        MySqlConnection connection;
+        MySqlCommand command;
+        MySqlDataReader reader;
         public BalanceAdministratorForm()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        DateTime localDate = DateTime.Now;
+
+        string myConnection = "Server=localhost;Database=proyek-kantin;Uid=root;pwd='';";
+
+        int finalBalance = 0;
+        int firstBalanceCount = 0;
+
+        private void LoadData()
+        {
+            string query = "SELECT * FROM topup WHERE tanggal_topup = "+localDate+"";
+            MySqlConnection connection = new MySqlConnection(myConnection);
+
+            try
+            {
+                connection.Open();
+                command = connection.CreateCommand();
+                command.CommandText = query;
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                dataGridView1.DataSource = dataSet.Tables[0].DefaultView;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+            }
+        }
+
+        protected void GetBalance(string userNis)
+        {
+            string plusBalance = tbBalance.Text.ToString();
+            string query = "SELECT saldo FROM pelanggan WHERE nis = "+userNis+"";
+
+            connection = new MySqlConnection(myConnection);
+            connection.Open();
+
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandText = query;
+                reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        string firstBalance = reader["saldo"].ToString();
+                        firstBalanceCount = int.Parse(firstBalance);
+
+                        if (int.Parse(plusBalance) < 5000)
+                        {
+                            MessageBox.Show("Minimum top up Rp.5000");
+                        }
+                        else
+                        {
+                            finalBalance = int.Parse(firstBalance) + int.Parse(plusBalance);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+            }
+        }
+
+        protected void SetBalance(string userNis)
+        {
+            string query = "UPDATE pelanggan set saldo=@balance WHERE nis = "+userNis+"";
+
+            connection = new MySqlConnection(myConnection);
+            connection.Open();
+
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandText = query;
+                command.Parameters.AddWithValue("@balance", finalBalance);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+            }
+        }
+
+        private void Btn_Confirm_Click(object sender, EventArgs e)
+        {
+            string userNum = tbUserNum.Text.ToString();
+
+            GetBalance(userNum);
+
+            if (finalBalance < firstBalanceCount)
+            {
+                MessageBox.Show("Transaksi Terkendala");
+            }
+            else if (finalBalance > firstBalanceCount)
+            {
+                SetBalance(userNum);
+                MessageBox.Show("Top Up Berhasil!!");
+            }
         }
     }
 }
